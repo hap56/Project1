f3673c1e1dc362ae64ee2a5196076f9e0d7d5792
harsh patel <hap56@afsconnect2.njit.edu>
2017-10-22 17:17:52 -0400

    calling get and post

--- index.php
+++ index.php
@@ -9,3 +9,24 @@ class Forautoloading
 }
  spl_autoload_register(array('Forautoloading','Autoload'));
 $csvtotable = new Csv_fetcher();
+
+class Csv_fetcher
+{
+	public function __construct()
+	{
+		$pageRequest = 'fileupload';
+		if(isset($_REQUEST['page']))
+		{
+			$pageRequest = $_REQUEST['page'];
+		}
+		$page = new $pageRequest;
+		if($_SERVER['REQUEST_METHOD'] == 'GET')
+		{
+			$page->Get();
+		}
+		else
+		{
+			$page->Post();
+		}
+	}
+}
