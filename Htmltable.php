2fac32f47cd3da96e524e15b0a2eb5433b094249
harsh patel <hap56@afsconnect2.njit.edu>
2017-10-22 17:59:57 -0400

    loops and conditions for converting csv to html table format

---
+++ Htmltable.php
@@ -0,0 +1,56 @@
+<?php
+ class Htmltable extends page 
+{
+  public function Get()
+ {
+    $row = 1;
+    if (($file = fopen($_GET['filename'], "r" )) !== false ) 
+  {
+     echo '<table border="1">';
+     while (($csvdata = fgetcsv($file, 1000, ",")) !== FALSE) 
+  {
+     $count = count($csvdata);
+    if ($row == 1) 
+  {
+      echo '<thead><tr>';
+   }
+    else
+  {
+      echo '<tr>';
+   }
+   
+     for ($initial=0; $initial < $count; $initial++)
+  {
+     if(empty($csvdata[$initial])) 
+  {
+     $filedata = "&nbsp;";
+   }
+     else
+  {
+     $filedata = $csvdata[$initial];
+   }
+     if ($row == 1) 
+  {
+     echo '<th>'.$filedata.'</th>';
+   }
+     else
+  {
+     echo '<td>'.$filedata.'</td>';
+   }
+   }
+     if ($row == 1) 
+  {
+     echo '</tr></thead><tbody>';
+   }
+       else
+  {
+       echo '</tr>';
+   }
+       $row++;
+   }
+       echo '</tbody></table>';
+       fclose($file);
+   }
+ }
+}
+?>
